45bcebc94adb86ecfa8b10da60f6281423deb78f
JoaoAzevedo184 <ccjconrad50@gmail.com>
2026-03-25 23:40:19 -0300

    foto de perfil

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import { theme } from "./config/theme";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
-import { About } from "./sections/About/About";
+import { About } from "./components/About";
 import { Stack } from "./components/Stack";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
